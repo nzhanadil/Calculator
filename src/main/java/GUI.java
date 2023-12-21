aa42aabb8c3cf4f333f7d2df76e3dcaa0b959a5a
Nuradil Zhanadil uulu <nuradilzhanadiluulu21@gmail.com>
2023-12-21 15:33:46 -0600

    some skeleton for GUI

---
+++ src/main/java/GUI.java
@@ -0,0 +1,52 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class GUI implements ActionListener {
+
+    int count = 0;
+    JLabel label;
+    JFrame frame;
+    JPanel panel;
+
+
+
+    public GUI(){
+        frame = new JFrame("Calculator");
+        panel = new JPanel();
+
+        panel.setBorder(BorderFactory.createEmptyBorder(30, 30, 10, 30));
+        panel.setLayout(new GridLayout(0,1));
+
+        JButton button = new JButton("Click me");
+        button.addActionListener(this);
+
+        label = new JLabel("Number of clicks: 0");
+
+
+        panel.add(button);
+        panel.add(label);
+
+
+
+        frame.add(panel, BorderLayout.CENTER);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+
+
+
+
+    }
+
+    public static void main(String[] args) {
+        new GUI();
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        count++;
+        label.setText("Number of clicks: "+count);
+    }
+}
---
+++ src/main/java/Main.java
@@ -0,0 +1,34 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        JFrame jFrame = new JFrame("Calculator");
+        jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        jFrame.setPreferredSize(new Dimension(600, 800));
+        jFrame.pack();
+        jFrame.setLocationRelativeTo(null);
+        jFrame.setVisible(true);
+
+
+
+
+        JButton button = new JButton("JBIB");
+        button.setBounds(50, 200, 100, 30);
+
+
+        JButton plusButton = new JButton("+");
+        plusButton.setBounds(50, 50, 100, 30);
+
+        JPanel panel = new JPanel();
+        panel.setBounds(10, 10, 200, 300);
+        panel.add(button);
+        panel.add(plusButton);
+
+
+        jFrame.add(panel);
+
+    }
+}
