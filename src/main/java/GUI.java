8a39c64b7c5123b3db14c079e7f57f94e2b10f99
Nuradil Zhanadil uulu <nuradilzhanadiluulu21@gmail.com>
2023-12-22 19:16:34 -0600

    some practice

--- src/main/java/GUI.java
+++ src/main/java/GUI.java
@@ -1,52 +1,106 @@
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 
-public class GUI implements ActionListener {
+public class GUI {
 
     int count = 0;
     JLabel label;
     JFrame frame;
     JPanel panel;
 
+    int value = 0;
 
+    public void createButtons() {
 
-    public GUI(){
+        for (int i = 0; i < 10; i++) {
+            JButton button = new JButton(String.valueOf(i));
+
+            button.addActionListener(this::setNumber);
+            panel.add(button);
+        }
+    }
+
+    private void setNumber(ActionEvent actionEvent) {
+        label.setText(label.getText() + actionEvent.getActionCommand());
+    }
+
+    public static void main(String[] args) {
+       GUI gui = new GUI();
+
+       gui.GUI();
+    }
+
+    public void GUI(){
         frame = new JFrame("Calculator");
         panel = new JPanel();
 
-        panel.setBorder(BorderFactory.createEmptyBorder(30, 30, 10, 30));
-        panel.setLayout(new GridLayout(0,1));
+        panel.setBorder(BorderFactory.createEmptyBorder(20, 20, 20, 20));
+        panel.setLayout(new GridBagLayout());
 
-        JButton button = new JButton("Click me");
-        button.addActionListener(this);
+        label = new JLabel(String.valueOf(count));
+        label.setSize(500, 100);
 
-        label = new JLabel("Number of clicks: 0");
+        JButton button = new JButton("MY");
 
+        GridBagConstraints c = new GridBagConstraints();
+        c.fill = GridBagConstraints.HORIZONTAL;
 
         panel.add(button);
         panel.add(label);
 
 
 
+
+        createButtons();
+
         frame.add(panel, BorderLayout.CENTER);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.pack();
+        frame.setMaximumSize(new Dimension(500, 700));
+        frame.setMinimumSize(new Dimension(500, 700));
+        frame.setVisible(true);
+
+    }
+
+
+
+    public GUI() {
+        frame = new JFrame("Calculator");
+        panel = new JPanel();
+
+        panel.setBorder(BorderFactory.createEmptyBorder(30, 30, 10, 30));
+        panel.setLayout(new GridLayout(10, 0));
+
+        label = new JLabel("0");
+
+
+        label.setForeground(Color.red);
+        label.setBackground(Color.black);
+        label.setSize(500, 100);
+
+        panel.add(label);
+
+
+
+
+
+//        createLabel();
+
+
+//        createLabel();
+        createButtons();
+
+        frame.add(panel, BorderLayout.CENTER);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setMaximumSize(new Dimension(500, 700));
+        frame.setMinimumSize(new Dimension(500, 700));
         frame.setVisible(true);
 
 
-
-
     }
 
-    public static void main(String[] args) {
-        new GUI();
-    }
 
-    @Override
-    public void actionPerformed(ActionEvent e) {
-        count++;
-        label.setText("Number of clicks: "+count);
-    }
+
 }
